e5c31f71c864d0bba84e9bb238880b9c34c6f0db
Girish Chaudhari <girishvishnuc98@gmail.com>
2021-12-01 19:33:57 +0530

    completed data binding

--- src/components/content/internal/products/index.js
+++ src/components/content/internal/products/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./style.scss";
-import { Container } from "@mui/material";
+import { Container, Skeleton } from "@mui/material";
 import { Link } from "react-router-dom";
-import { data } from "../../../../data";
-import { useDispatch } from "react-redux";
+// import { data } from "../../../../data";
+import { useDispatch, useSelector } from "react-redux";
 import { productsView } from "../../../../redux/actions/products";
 
 const GetAllProducts = () => {
   const dispatch = useDispatch();
+  const data = useSelector((state) => state.products);
+  console.log("this is selector data", data.products);
+
+  const skeletonLoop = [1, 2, 3, 4, 5, 6, 7, 8];
 
   useEffect(() => {
     console.log("hello");
@@ -17,25 +21,47 @@ const GetAllProducts = () => {
   return (
     <>
       <Container>
-        <div className="cards-box">
-          {data.map((item, i) => (
-            <div className="card" key={i}>
-              <div className="card-img">
-                <img src={item.img_src} alt="" />
-              </div>
-              <div className="desc-box">
-                <div className="title">{item.prod_title}</div>
-                <div className="price-section">$ {item.price}</div>
-                <div className="seller">
-                  <span className="seller-title">Seller:</span> {item.seller}
+        {data.products.length === 0 ? (
+          <div className="cards-box">
+            {skeletonLoop.map((item, i) => (
+              <div className="card" key={i}>
+                <div className="card-img-skeleton">
+                  <Skeleton height={120} width="100%"></Skeleton>
                 </div>
-                <Link to={`singlepage/${item.prod_id}`}>
-                  <button className="cart-btn">Add To Cart</button>
-                </Link>
+                <div className="desc-box">
+                  <div className="title">
+                    <Skeleton width="100%"></Skeleton>
+                    <Skeleton width="100%"></Skeleton>
+                  </div>
+                  <div className="price-section">
+                    <Skeleton width="50%"></Skeleton>
+                  </div>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className="cards-box">
+            {data.products.map((item, i) => (
+              <div className="card" key={i}>
+                <div className="card-img">
+                  <img src={item.image} alt="" />
+                </div>
+                <div className="desc-box">
+                  <div className="title">{item.title}</div>
+                  <div className="price-section">$ {item.price}</div>
+                  <div className="seller">
+                    <span className="seller-title">Seller:</span> Girish
+                    Chaudhari
+                  </div>
+                  <Link to={`singlepage/${item.id}`}>
+                    <button className="cart-btn">Add To Cart</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </Container>
     </>
   );
